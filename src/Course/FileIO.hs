713c591c308b9a33f8138a84b95a02069985f6ee
Frank <b439988l@gmail.com>
2019-07-12 17:06:09 +1000

    missing main

--- src/Course/FileIO.hs
+++ src/Course/FileIO.hs
@@ -100,30 +100,30 @@ printFiles = \list ->
 getFile ::
   FilePath
   -> IO (FilePath, Chars)
-getFile =
-  error "todo: Course.FileIO#getFile"
+getFile = \fp -> (\chars -> (fp, chars))<$> readFile fp
+
 
 -- Given a list of file names, return list of (file name and file contents).
 -- Use @getFile@.
 getFiles ::
   List FilePath
   -> IO (List (FilePath, Chars))
-getFiles =
-  error "todo: Course.FileIO#getFiles"
+getFiles = 
+  \list -> sequence (getFile <$> list)
 
 -- Given a file name, read it and for each line in that file, read and print contents of each.
 -- Use @getFiles@ and @printFiles@.
 run ::
   FilePath
   -> IO ()
-run =
-  error "todo: Course.FileIO#run"
+run = \fp -> readFile fp >>= \chrs ->
+  getFiles (lines chrs) >>= \x ->
+  printFiles x
 
 -- /Tip:/ use @getArgs@ and @run@
 main ::
   IO ()
-main =
-  error "todo: Course.FileIO#main"
+main = 
 
 ----
 
